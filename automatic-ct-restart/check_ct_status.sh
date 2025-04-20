4c72825e0fead4a583defcb0720b795c83de2e67
Stephan <23691468+stephansilberberg@users.noreply.github.com>
2025-04-20 21:30:40 +0200

    Update check_ct_status.sh

--- automatic-ct-restart/check_ct_status.sh
+++ automatic-ct-restart/check_ct_status.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# CT IDs to start
-#CTS=(101 130 150)
+# CT IDs to check
+# Example: CTS=(100 102 103)
 CTS=(101 130 150)
 for CTID in "${CTS[@]}"; do
-  # Check if the CT is stopped
+  # Check CT status
   STATUS=$(/usr/sbin/pct status $CTID | awk '{print $2}')
+  # If stopped restart the CT
   if [ "$STATUS" == "stopped" ]; then
     /usr/sbin/pct start $CTID
     echo "Started CT $CTID"
